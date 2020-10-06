050c89e9133d3c057f5e8e3dfa49e47f747c29e8
tek poudel <tekpoudel@teks-MacBook-Pro.local>
2020-10-06 15:34:14 -0400

    files modified

---
+++ main.js
@@ -0,0 +1,43 @@
+let roll = document.querySelector(".Roll");
+let totalresult = document.querySelector("#total");
+let showtherolls = document.querySelector(".showtherolls");
+let lists = document.querySelector("#lists");
+let numofdice = document.querySelector("#number-of-dice");
+let restarts = document.querySelector("#reset-button");
+  
+let dierolls = [];
+    
+let counter = 0;
+roll.addEventListener("click", function(){
+let dicenum = numofdice.value
+let total = 0;
+while (counter<dicenum) {
+    let x = Math.floor(Math.random()*6 +1)
+    dierolls.push(x)
+    total +=x
+    counter = counter +1
+    
+}
+totalresult.innerHTML=total
+})
+
+showtherolls.addEventListener("click", function(){
+    str = "<ol>";
+    dierolls.forEach(function (dicenum){
+        str += "<li>" + dicenum + "</li>";
+    });
+    str +="</ol>";
+    lists.innerHTML=str
+});
+
+restarts.addEventListener("click", function() {
+    totalresult.innerHTML = 0;
+    numofdice.value =0;
+    lists.innerHTML = '';
+});
+
+
+
+
+
+
